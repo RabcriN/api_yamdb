7bbfb0855beaf06ccaa6927b182e8b913d3e284f
Татьяна Говорина <tratatatanya@gmail.com>
2022-09-25 01:51:57 +0300

    permissions refactored

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,30 +4,41 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_anonymous:
-            return False
-        if request.user.is_admin:
-            return True
+        return request.method in permissions.SAFE_METHODS or (
+            request.user.is_authenticated
+            and (request.user.is_admin or request.user.is_superuser)
+        )
 
 
-class IsModeratorOrReadOnly(permissions.BasePermission):
+class IsAuthorModeratorAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_anonymous:
-            return False
-        if request.user.is_moderator:
-            return True
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_admin
+            or request.user.is_moderator
+            or obj.author == request.user
+        )
 
 
 class IsAdminOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.is_anonymous:
-            return False
-        if request.user.is_admin:
-            return True
+        return request.user.is_authenticated and (
+            request.user.is_admin or request.user.is_superuser
+        )
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            (request.method in permissions.SAFE_METHODS)
+            or obj.author == request.user
+        )

95e3de508aecb53a3693fcc86eaa22dec55b2bac
unknown <user@DESKTOP-VI1L6FA.office.tut.ru>
2022-09-25 11:18:03 +0300

    test

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -61,9 +61,8 @@ class CommentViewSet(viewsets.ModelViewSet):
         return review.comments.all()
 
     def perform_create(self, serializer):
-        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
-        serializer.save(author=self.request.user, review=review, id=title.id)
+        serializer.save(author=self.request.user, review=review)
 
 
 class SignUpViewSet(viewsets.ModelViewSet):
